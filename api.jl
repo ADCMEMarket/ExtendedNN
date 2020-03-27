9be4dc6299933ea9fbe066cf1341db5f5e3b3b59
Kailai Xu <kailaix@stanford.edu>
2020-03-26 21:26:41 -0700

    remove api

--- api.jl
+++
@@ -1,7 +0,0 @@
-function extended_nn(x,config,theta,activation)
-    extended_nn_ = load_op_and_grad("./build/libExtendedNn","extended_nn", multiple=true)
-    x,config_,theta = convert_to_tensor([x,config,theta], [Float64,Int64,Float64])
-    u, du = extended_nn_(x,config_,theta,activation)
-    n = length(x)÷config[1]
-    reshape(u, (n, config[end])), du 
-end
